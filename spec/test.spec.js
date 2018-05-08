05139a9f8b50be4aac66616b4d77d2b68bf7d8f0
ptavarez16 <ptavarez16@yahoo.com>
2018-05-08 19:15:31 -0400

    Add test.spec.js file to practice using jasmine

---
+++ spec/test.spec.js
@@ -0,0 +1,15 @@
+describe('Hello World', function () {
+	it('Hello Test', function() {
+		console.log('Testing the Test')
+	})
+	
+	it('Hello Test Again', function() {
+		console.log('Testing the Test... Again')
+	})
+	
+	describe('Hello World Part II', function () {
+		it('Hello Test', function() {
+			console.log('Testing the Test Part II')
+		})
+	})
+})
