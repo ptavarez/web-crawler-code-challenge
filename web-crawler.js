95ff7f6261c01037f97f1ea4ee9f8d3b3c11df84
ptavarez16 <ptavarez16@yahoo.com>
2018-06-21 22:47:55 -0400

    Challenge update - Solved the challenge the correct way by using recursion

--- web-crawler.js
+++ web-crawler.js
@@ -1,39 +1,53 @@
-Array.prototype.diff = function(a) {
-  return this.filter(function(i) {return a.indexOf(i) < 0})
-}
+const internet = require('./lib/internet/internetOne')
 
-const unique = function (value, index, self) {
-      return self.indexOf(value) === index;
-}
+const pageOne = internet.pages[0].address
 
-const webCrawler = function (internet) {
-  
-  let addressArr = []
-  let linksArr = []
-  let skippedDup = []
+let succeed = []
+let skip = []
+let error = []
+
+const webCrawler = function (address, internet) {
   
   for (let i = 0; i < internet.length; i++) {
-    let address = internet[i].address
-    let links = internet[i].links
-    
-    addressArr.push(address)
-    
-    for (let j = 0; j < links.length; j++) {
-      linksArr.push(links[j])
-    }
   
-    success = addressArr.diff(addressArr.diff(linksArr))
-    error = (linksArr.diff(success))
-    
-    for (let k = 0; k < links.length; k++) {
-      if (success.indexOf(links[k]) > -1) {
-        skippedDup.push(links[k])
+    if (address === internet[i].address) {
+      // If the address originally invoked is the current address on the internet,
+      // add it to the list of successes.
+      succeed.push(address)
+      
+      let links = internet[i].links
+      
+      // Given the list of links on that address,
+      // check them.
+      for (let j = 0; j < links.length; j++) {
+        // If this link isn't the current address we're iterating, and it's not
+        // already on the list of successes...
+        if (links[j] !== internet[i].address && succeed.indexOf(links[j]) === -1) {
+          // Check  to see if any address matches it.
+          // If not, add it to the list of errors.
+          if (internet.find((current) => {
+            return current.address === links[j]
+          }) === undefined && error.indexOf(links[j]) === -1) {
+            error.push(links[j])
+            // Otherwise, it must exist on the internet but not be listed on the succeses,
+            // so call web crawler on it.
+          } else {
+            webCrawler(links[j], internet)
+          }
+          // Check to see if it's already on the list of succeses.
+          // If so, add it to the list of skips.
+        } else if (succeed.indexOf(links[j]) !== -1 && skip.indexOf(links[j]) === -1) {
+          skip.push(links[j])
+        }
       }
     }
-    
-    skipped = skippedDup.filter(unique)
   }
-  return [success, skipped, error]
+  return [succeed, skip, error]
 }
 
-module.exports = webCrawler
+module.exports = {
+  webCrawler,
+  succeed,
+  skip,
+  error
+}
